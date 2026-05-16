f66f11e326de83e3b9aea3b105f78471d7da1fed
vukhanh2005 <nvk12a4@gmail.com>
2026-05-17 01:48:44 +0700

    remake UI/UX

--- front-end/src/components/Product.jsx
+++ front-end/src/components/Product.jsx
@@ -1,7 +1,29 @@
-import { getProducts } from "../services/api";
-
 // product là dữ liệu sản phẩm dạng json
-function Product({product}) { 
+function Product({product}) {
+    const price = Number(product.price);
+    const oldPrice = Number(product.oldPrice ?? product.old_price);
+
+    function renderPrice() {
+        if (price < 0) {
+            return <p>Vui lòng liên hệ để biết thêm chi tiết!</p>;
+        }
+
+        if (price === 0) {
+            return <p>FREE</p>;
+        }
+
+        if (oldPrice > price) {
+            return (
+                <>
+                    <p className="old-price">Giá cũ: {oldPrice}</p>
+                    <p>Giá mới: {price}</p>
+                </>
+            );
+        }
+
+        return <p>Giá: {price}</p>;
+    }
+
     return ( 
         <div className="product-wrapper">
             <a href="">
@@ -12,15 +34,7 @@ function Product({product}) {
                     <p><b>{product.name}</b></p>
                 </div>
                 <div className="price-product-box">
-                    {
-                        product.old_price < 0 || <p>Vui lòng liên hệ để biết thêm chi tiết!</p>
-                    }
-                    {
-                        (product.old_price == product.price || product.old_price < product.price) && product.price >= 0?
-                        <p>Giá: {product.price}</p>
-                        :
-                        <p>Giá: {product.price}</p>
-                    }
+                    {renderPrice()}
                 </div>
             </a>
         </div>
